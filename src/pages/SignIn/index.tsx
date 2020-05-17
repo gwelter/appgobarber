1ad5b44a40bb9dc6661731388e4208c205b9ef41
Guilherme André Welter <guilherme.a.welter@gmail.com>
2020-05-17 11:57:04 -0300

    Using external fonts

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Image } from 'react-native';
+import { Container, Title } from './styles';
 
-import { Container } from './styles';
+import logoImg from '../../assets/logo.png';
 
 export const SignIn: React.FC = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Image source={logoImg} />
+      <Title>Faça seu logon</Title>
+    </Container>
+  );
 };
